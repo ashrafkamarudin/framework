47e0b24bfae81754b340daeabdb33f97cb214831
ashrafkamarudin <ashrafkamarudin1995@gmail.com>
2018-10-14 17:21:12 +0800

    first setup

---
+++ app/route.php
@@ -0,0 +1,21 @@
+<?php
+
+// Defining routes, from specific...
+$app->get('/hello(/:name)', function ($name = 'anonymous') use ($app, $log) {
+    $greeter = new SampleApp\Helpers\Hello($name);
+    echo $greeter->greet();
+    $log->info("Just logging $name visit...");
+});
+
+$app->get('/about', function () use ($app, $log) {
+    echo "<h1>About ", $app->config('name'), "</h1>";
+    echo "<pre>";
+    var_dump($_SERVER);
+    echo "</pre>";
+    echo "<p><small>Current mode is: ", $app->config('mode'), '</small></p>';
+});
+
+// To generic
+$app->get('/', function () use ($app, $log) {
+    echo '<h1>Welcome to ', $app->config('name'), '</h1>';
+});
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+// Include application bootstrap
+require_once dirname(__FILE__) . '/../app/lib/bootstrap.php';
+
+// Include applcation routes
+require_once dirname(__FILE__) . '/../app/route.php';
+
+// Important: run the app ;)
+$app->run();
